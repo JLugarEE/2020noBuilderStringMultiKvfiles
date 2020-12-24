8d382629017dc0a370cb650cc01f3ec409341511
JLugarEE <lugarjohn@hotmail.com>
2020-12-24 14:13:17 -0500

    Fixed good.screen.receive.py grabbed poor copy

--- good/screens/receive.py
+++ good/screens/receive.py
@@ -2,31 +2,32 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
-from kivy.utils import platform
-
 from kivymd.uix.button import MDFlatButton
 
 import socket
+import threading
 import pathlib
-from struct import pack
+import plyer
+from struct import unpack
 
 Builder.load_string("""
-
-<SendScreen>:
-    id: send_screen
-    name: 'send_screen'
+<ReceiveScreen>:
+    id: receive_screen
+    name: 'receive_screen'
     MDRaisedButton:
-        text: "Select File to Send"
+        text: 'Waiting to Receive File'
+        font_style: 'H3'
         elevation: 10
         pos_hint: {'center_x': 0.5, 'center_y': 0.5}
         on_release: app.sm.current = 'main_screen'
+
 """)
 
-#Builder.load_file('send.kv')
+#Builder.load_file('receive.kv')
 
-
-class SendScreen(Screen):
+class ReceiveScreen(Screen):
 
     def __init__(self, *args, **kwargs):
-        super(SendScreen, self).__init__(*args, **kwargs)
+        super(ReceiveScreen, self).__init__(*args, **kwargs)
+
 
